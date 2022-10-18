64005f3f2c87ed1f466fb414a6055bc1ca117c1f
guishas <lunettagui@gmail.com>
2022-10-18 19:43:44 -0300

    api pronta, falta decoração da documentação

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ app = FastAPI()
 
 banco: List[Produto] = []
 
-@app.get("/")
-def root():
-    return {"message": "root endpoint, please access other endpoints"}
-
 @app.get("/produtos")
 def get_products():
     return banco
@@ -18,13 +14,39 @@ def get_products():
 @app.get("/produtos/{product_id}")
 def get_product_by_id(product_id: str):
     for produto in banco:
-        if produto.product_id == product_id:
-            return produto.dict()
+      if str(produto["product_id"]) == product_id:
+          return produto
 
-    return {"erro": "id não existe na base de dados"}
+    return {"erro": "produto não existe na base de dados"}
 
 @app.post("/produtos")
 def create_product(produto: Produto):
     produto.product_id = uuid.uuid4()
     banco.append(produto.dict())
     return produto.dict()
+
+@app.put("/produtos")
+def update_produto(produto: Produto):
+  prod = None
+  print(produto)
+  for p in banco:
+    if str(p["product_id"]) == str(produto.product_id):
+      prod = produto
+
+  if prod == None:
+    return {"erro": "produto não existe na base de dados"}
+  
+  prod.name = produto.name
+  prod.description = produto.description
+  prod.price = produto.price
+
+  return prod.dict()
+
+@app.delete("/produtos/{product_id}")
+def delete_produto(product_id: str):
+  for produto in banco:
+    if str(produto["product_id"]) == product_id:
+      banco.remove(produto)
+      return {"message": "produto removido com sucesso"}
+
+  return {"erro": "produto não existe na base de dados"}
